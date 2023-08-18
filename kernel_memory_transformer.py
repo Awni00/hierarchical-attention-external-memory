ced5c72d15d4704072570748eaec2112736b1851
awni <awnyaltabaa@gmail.com>
2023-08-18 18:37:47 -0400

    added kernel_memory_transformer.py implements kernel-memory transformer model

---
+++ kernel_memory_transformer.py
@@ -0,0 +1,92 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+from transformer_modules import AddPositionalEmbedding, MemoryAddPositionalEmbedding
+from attention import GlobalSelfAttention, CausalSelfAttention, CrossAttention
+
+class KernelMemoryTransformer(tf.keras.Model):
+    def __init__(self, vocab_size, embedding_dim, key_dim, n_heads, ff_sizes, **kwargs):
+        super().__init__(**kwargs)
+
+        self.alpha_it = None
+        self.embedder = layers.Embedding(vocab_size, embedding_dim, name='embedder')
+        self.pos_embedder_adder  = AddPositionalEmbedding(name='pos_embedder_adder')
+        self.memory_pos_embedder_adder = MemoryAddPositionalEmbedding(name='mem_pos_embedder_adder')
+
+        # different self-attention layers for input and memories for encoding sequences
+        self.input_self_attention = CausalSelfAttention(num_heads=n_heads, key_dim=key_dim, value_dim=embedding_dim//n_heads, name='self_attn')
+        self.memory_self_attention_layer = GlobalSelfAttention(num_heads=n_heads, key_dim=key_dim, value_dim=embedding_dim//n_heads, name='mem_self_attn')
+
+        # create cross attention layer
+        self.cross_attention_layer = CrossAttention(num_heads=n_heads, key_dim=key_dim, value_dim=embedding_dim//n_heads, name='cross_attn')
+
+        self.feedforward = tf.keras.Sequential([layers.Dense(z, activation='relu') for z in ff_sizes], name='feedforward')
+        self.output_dense = layers.Dense(vocab_size, activation='softmax', name='output')
+
+    def memory_self_attention(self, mem_seqs):
+        '''apply self-attention to each memory sequence'''
+        lambda_fn = lambda x: self.memory_self_attention_layer(x)
+        return tf.map_fn(lambda_fn, mem_seqs)
+
+    def get_cross_probs(self, embedded_input_seqs, embedded_mem_x_seqs):
+        '''compute cross attention probabilities prob(x_{i+1} = . | x_{1:i}, x^{(t)}) for each memory'''
+
+        def get_memory_cross_probs(Et):
+            cross_attention_output = self.cross_attention_layer(
+                x=embedded_input_seqs,
+                context=Et)
+            return cross_attention_output # shape: (batch_size, seq_len, mem_len)
+
+        # compute cross-attention for each memory
+        cross_probs = tf.map_fn(get_memory_cross_probs, tf.transpose(embedded_mem_x_seqs, perm=[1, 0, 2, 3]))
+        cross_probs = tf.transpose(cross_probs, perm=[1, 0, 2, 3]) # shape: (batch_size, n_mem, mem_length, embedding_dim)
+
+        # compute cross-probs from cross-attention via feedforward network
+        cross_probs = self.feedforward(cross_probs)
+        cross_probs = self.output_dense(cross_probs) # shape: (batch_size, n_mems, mem_length, vocab_size)
+
+        return cross_probs
+
+    def get_observed_cross_probs(self, cross_probs, seq_input):
+        '''given the cross_probs tensor, extract the components coresponding to the observed sequence'''
+
+        cross_probs_observed = tf.transpose(cross_probs, perm=(1,0,2,3))
+        cross_probs_observed = tf.map_fn(lambda x: tf.gather(x, indices=seq_input, axis=-1, batch_dims=2), cross_probs_observed)
+        cross_probs_observed = tf.transpose(cross_probs_observed, perm=(1,0,2))
+        return cross_probs_observed
+
+    def compute_mem_attention_scores(self, cross_probs_observed):
+        '''compute memory-level attention scores, alpha_it (i is position in input sequence, t is memory index)'''
+
+        log_cross_probs_observed = tf.math.log(cross_probs_observed)
+        w_it = tf.math.cumsum(log_cross_probs_observed, axis=-1, exclusive=True)
+        alpha_it = tf.nn.softmax(w_it, axis=1) # shape [batch_size, num_mems, in_seq_length]
+        return alpha_it
+
+    def call(self, inputs):
+        seq_input, memory_input = inputs
+
+        # embed input sequences and memory sequences
+        embedded_input_seqs = self.embedder(seq_input)
+        embedded_mem_x_seqs = self.embedder(memory_input)
+
+        # add positional embeddings
+        embedded_input_seqs = self.pos_embedder_adder(embedded_input_seqs)
+        embedded_mem_x_seqs = self.memory_pos_embedder_adder(embedded_mem_x_seqs)
+
+        # encode via self-attention
+        embedded_input_seqs = self.input_self_attention(embedded_input_seqs) # shape: (batch_size, input_length, embedding_dim)
+        embedded_mem_x_seqs = self.memory_self_attention(embedded_mem_x_seqs) # shape: (batch_size, num_memories, memory_length, embedding_dim)
+
+        # compute prob(x_{i+1} = . | x_{1:i}, x^{(t)}) for each memory
+        cross_probs = self.get_cross_probs(embedded_input_seqs, embedded_mem_x_seqs)
+
+        # compute prob(x_{i+1} | x_{1:i}, x^{(t)}) for each memory
+        cross_probs_observed = self.get_observed_cross_probs(cross_probs, seq_input)
+
+        # compute alpha_it = softmax(w_it) where w_it = sum_{j=1}^{i-1} log prob(x_{j+1} | x_{1:j}, x^{(t)})
+        alpha_it = self.compute_mem_attention_scores(cross_probs_observed)
+
+        # compute prob(x_{i+1} | x_{1:i}, {x^{(t)}}_t) = \sum_t alpha_it * prob(x_{i+1} = . | x_{1:i}, x^{(t)})
+        output = tf.reduce_sum(tf.multiply(tf.expand_dims(alpha_it, axis=-1), cross_probs), axis=1)
+
+        return output
