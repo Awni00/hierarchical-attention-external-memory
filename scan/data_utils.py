2b00331bf4f00de6d55c1d5d4c6d2756e3b02648
Awni <awnyaltabaa@gmail.com>
2023-11-05 22:10:21 -0500

    updated date_utils added utils for loading and processing memoryful datasets

--- scan/data_utils.py
+++ scan/data_utils.py
@@ -75,27 +75,32 @@ def load_scan_ds(split):
 
     return train_ds, test_ds, command_vectorizer, action_vectorizer
 
-def sample_memory_buffer(input_seqs, target_seqs, n_mems):
+def sample_memory_buffer(input_seqs, target_seqs, n_mems, n=None):
     '''given input, target seqs, sample n_mems for each training example'''
 
-    n = len(input_seqs)
-    assert n == len(target_seqs)
+    if n is None:
+        n = len(input_seqs)
+        assert n == len(target_seqs)
     sample = np.array([np.random.choice(n, n_mems) for _ in range(n)])
     input_mem_seqs = tf.gather(input_seqs, sample, axis=0)
     target_mem_seqs = tf.gather(target_seqs, sample, axis=0)
 
     return input_mem_seqs, target_mem_seqs
 
-def create_memory_ds(train_ds, n_mems):
+def create_memory_ds(base_ds, n_mems, memory_bank=None):
     '''
     given input, target, label seqs in tf dataset, sample memory buffer
     for each training example and create new dataset including memories
     '''
+    if memory_bank is None:
+        memory_bank = base_ds
 
-    input_seqs, target_seqs, label_seqs = unravel_ds(train_ds, format='s,t,l')
-    input_mem_seqs, target_mem_seqs = sample_memory_buffer(input_seqs, target_seqs, n_mems=n_mems)
-    mem_train_ds = tf.data.Dataset.from_tensor_slices((((input_seqs, target_seqs), (input_mem_seqs, target_mem_seqs)), label_seqs))
-    return mem_train_ds
+    input_seqs, target_seqs, label_seqs = unravel_ds(base_ds, format='s,t,l')
+
+    mem_input_bank, mem_target_bank, _ = unravel_ds(memory_bank, format='s,t,l')
+    input_mem_seqs, target_mem_seqs = sample_memory_buffer(mem_input_bank, mem_target_bank, n_mems=n_mems, n=len(input_seqs))
+    mem_ds = tf.data.Dataset.from_tensor_slices((((input_seqs, target_seqs), (input_mem_seqs, target_mem_seqs)), label_seqs))
+    return mem_ds
 
 def unravel_ds(ds, format='s,t'):
     if format == 's,t':
@@ -109,3 +114,11 @@ def unravel_ds(ds, format='s,t'):
         target = np.array([y.numpy() for (x, y), z in ds])
         label = np.array([z.numpy() for (x, y), z in ds])
         return source, target, label
+
+def unravel_mem_ds(mem_ds):
+    source = np.array([s for (((s, t), (sm, tm)), l) in mem_ds])
+    target = np.array([t for (((s, t), (sm, tm)), l) in mem_ds])
+    mem_source = np.array([sm for (((s, t), (sm, tm)), l) in mem_ds])
+    mem_target = np.array([tm for (((s, t), (sm, tm)), l) in mem_ds])
+    label = np.array([l for (((s, t), (sm, tm)), l) in mem_ds])
+    return source, target, mem_source, mem_target, label
